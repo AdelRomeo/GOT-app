0b368d3c7a94a720fda52c13b192ca4e9d51f9bb
AdelRomeo <11cage11@mail.ru>
2021-05-01 23:26:24 +0700

    itemList p1

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,20 +1,58 @@
 import React, {Component} from 'react';
 import './itemList.css';
+import GotService from "../../services/gotService";
+import Spinner from "../spinner/spinner";
+
 export default class ItemList extends Component {
 
-    render() {
-        return (
-            <ul className="item-list list-group">
-                <li className="list-group-item">
-                    John Snow
-                </li>
-                <li className="list-group-item">
-                    Brandon Stark
-                </li>
-                <li className="list-group-item">
-                    Geremy
-                </li>
-            </ul>
-        );
+  gotService = new GotService();
+
+  state = {
+    charList: null
+  }
+
+  //получем список персонажей
+  componentDidMount() {
+    this.gotService.getAllCharacters()
+      .then((charList) => {
+        this.setState({
+          charList
+        })
+        console.log(charList)
+      })
+  }
+
+  renderCharList = (arr) => {
+    return arr.map((char, i) => {
+      console.log(i)
+      return (
+        <li
+          key={i}
+          className="list-group-item"
+          //onClick={this.props.onCharFullInfo(i)}
+        >
+          {char.name}
+        </li>
+      )
+    })
+  }
+
+  render() {
+
+    //массив объектов персонажей
+    const {charList} = this.state;
+
+    if (!charList) {
+      return <Spinner/>
     }
+
+    //верстка для рендера
+    const items = this.renderCharList(charList);
+
+    return (
+      <ul className="item-list list-group">
+        {items}
+      </ul>
+    );
+  }
 }
